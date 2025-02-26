4fd7f0b4a25cbc141e94fba9c1fe79fef68aa35a
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-02-26 11:44:15 +0900

    productをスライドで表示できるようにした

--- src/components/element/productList/ProductList.tsx
+++ src/components/element/productList/ProductList.tsx
@@ -1,12 +1,46 @@
+"use client";
 import { Product } from "../product/Product";
 import styles from "./ProductList.module.scss";
 import { ProductStack } from "@/const/productstack/ProductStack";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 export const ProductList = () => {
     return (
-        <div className={styles.list}>
-            {ProductStack.map((element, index) => {
-                return <Product key={index} description={element} />;
-            })}
-        </div>
+        <>
+            {/* <div className={styles.list}>
+                {ProductStack.map((element, index) => {
+                    return <Product key={index} description={element} />;
+                })}
+            </div> */}
+
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={15}
+                pagination={{
+                    clickable: true,
+                }}
+                breakpoints={{
+                    700: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                }}
+                centeredSlides={true}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                {ProductStack.map((element, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <Product description={element} />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </>
     );
 };
