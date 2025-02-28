ca9645a01209a206efbd60dfa6d93c2bd5765045
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-02-28 23:35:48 +0900

    Aboutmeの自己紹介のところを作成

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,7 +1,12 @@
+import { Aboutme } from "@/components/element/aboutme/Aboutme";
+import { Title } from "@/components/element/title/Title";
+
 export default function Page() {
     return (
         <div>
-            <h1>Hello</h1>
+            <Title text="Aboutme" color="#FF5B12" />
+            <Aboutme />
+            <Title text="History" color="#FF5B12" />
         </div>
     );
 }
