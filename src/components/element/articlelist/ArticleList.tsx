2928eefd41b498f38abaf0432c26d5ebdecdc2aa
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-12-18 10:33:48 +0900

    ArticleListコンポーネントのスタイルを更新し、ArticleCardを使用するように変更

--- src/components/element/articlelist/ArticleList.tsx
+++ src/components/element/articlelist/ArticleList.tsx
@@ -1,21 +1,23 @@
-import { Card } from "../card/Card";
+import ArticleCard from "@/components/element/article/ArticleCard";
 import { Contentslist } from "../contentslist/Contentslist";
 import { client } from "@/libs/client";
 import { Article } from "@/types/article";
 
 export const ArticleList = async () => {
     const blog = await client.get({ endpoint: "article" });
-    
-    const articles: Article[] = blog.contents.map(
-        ({ id, eyecatch, title, tag,contents,publishedAt  }: Article) => ({
-            id,
-            imgUrl: eyecatch?.url || '',
-            title,
-            tag: tag?.[0] ? [tag[0]] : ['未分類'],
-			contents,
-			publishedAt,
-        })
-    );
+
+    const articles: Article[] = (blog.contents as Article[]).map((c) => ({
+        id: c.id,
+        eyecatch: {
+            url: c.eyecatch?.url ?? '',
+            height: c.eyecatch?.height ?? '',
+            width: c.eyecatch?.width ?? '',
+        },
+        title: c.title || '',
+        tag: c.tag?.length ? c.tag : undefined,
+        contents: c.contents,
+        publishedAt: c.publishedAt || '',
+    }));
 
     const articleLength = articles.length;
     const MAX_DISPLAY = 4;
@@ -26,19 +28,21 @@ export const ArticleList = async () => {
 
     return (
         <Contentslist text="ブログ" more={hasMore ? 'blog' : ''}>
-	            {displayArticles.map((article) => (
-                <Card
-                    key={article.id}
-                    text={article.title}
-                    url={`blog/${article.id}`}
-                />
+                    {displayArticles.map((article) => (
+                <ArticleCard key={article.id} article={article} />
             ))}
 			{/* 最大表示数に満たない場合はダミーのカードを表示 */}
             {emptySlots > 0 && Array.from({ length: emptySlots }).map((_, index) => (
-                <Card 
-                    key={`coming-soon-${index}`} 
-                    text="Coming soon..." 
-                    url="#"
+                <ArticleCard
+                    key={`coming-soon-${index}`}
+                    article={{
+                        id: `coming-soon-${index}`,
+                        eyecatch: { url: '/not.png', height: '', width: '' },
+                        title: 'Coming soon...',
+                        tag: undefined,
+                        contents: '',
+                        publishedAt: '',
+                    }}
                 />
             ))}
         </Contentslist>
