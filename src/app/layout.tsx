c03b35e5f973b8f0fd42b748e180eeca8a6f1d25
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-12-27 20:31:30 +0900

    metadataにdomainのURLを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,23 @@ const notoSansJp = Noto_Sans_JP({ subsets: ["latin"], variable: "--font-noto" })
 export const metadata: Metadata = {
     title: "mimimimimimi's portfolio",
     description: "mimimimimimi is created",
+    metadataBase: new URL("https://bike-bun-bun.com"),
+    openGraph: {
+        locale: "ja_JP",
+        type: "website",
+        siteName: "mimimimimimi's portfolio",
+        title: "mimimimimimi's portfolio",
+        description: "【超絶必見】俺のポートフォリオサイトですよ",
+        url: new URL("https://bike-bun-bun.com"),
+    },
+    twitter: {
+        card: "summary_large_image",
+        creatorId: "mimimimimimi",
+        creator: "@Atui_yaroo",
+    },
+    verification: {
+        google: process.env.GOOGLE_VERIFICATION || "",
+    },
 };
 
 export default function RootLayout({
