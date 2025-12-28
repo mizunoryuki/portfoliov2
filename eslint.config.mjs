29db5578364f0646bea20c6cc12b14a8aed061cc
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-12-28 21:58:41 +0900

    eslintの設定を追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,9 +15,84 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = defineConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts",".vercel/**"]),
-]);
+export default tseslint.config([
+  js.configs.recommended,
+  ...compat.extends(
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+    "next/typescript",
+  ),
 
-export default eslintConfig;
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "jsx-a11y": jsxA11y,
+      "unused-imports": unusedImports, // 追加
+    },
+    rules: {
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/self-closing-comp": "error",
+      "@next/next/no-img-element": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "import/no-default-export": "error",
+      "react/jsx-no-duplicate-props": ["error", { ignoreCase: true }],
+      "no-dupe-keys": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      ...jsxA11y.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+
+  {
+    files: [
+      "src/app/**/{page,layout,template,not-found,error,loading,route,default}.tsx",
+      "next.config.mjs",
+      "eslint.config.mjs",
+    ],
+    rules: {
+      "import/no-default-export": "off",
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      ".vercel/**",
+      "public/**",
+    ],
+  },
+]);
