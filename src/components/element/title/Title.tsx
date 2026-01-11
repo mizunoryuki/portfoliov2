a20a65be62ee474eb1f458028a270a199316cb87
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-01-11 13:41:14 +0900

    change title component font

--- src/components/element/title/Title.tsx
+++ src/components/element/title/Title.tsx
@@ -5,7 +5,7 @@ import styles from "./Title.module.scss";
 export const Title = ({ text, color = "gray" }: TitleProps) => {
   return (
     <div className={styles.titleContainer}>
-      <h1>{text}</h1>
+      <h1 className={styles.title}>{text}</h1>
       <div style={{ backgroundColor: color }} />
     </div>
   );
